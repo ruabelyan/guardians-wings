99bca46cbc85cc3d2fc5dce1972b6c9389c6d105
Ruben <ru.abelyan@mail.ru>
2022-03-03 18:58:00 +0400

    fixed probels

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -9,7 +9,7 @@ import RecentPosts from '../../components/RecentPosts'
 
 
 const blogs = [
-    {
+    { 
         image: '/images/blog-page/1.jpg',
         title: 'What lawyer can do for you',
         text: 'I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.',
