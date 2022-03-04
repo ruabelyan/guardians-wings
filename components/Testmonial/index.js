a5b4779c0d11554835868eb5f0c1ee75b9e0a32f
Ruben <ru.abelyan@mail.ru>
2022-03-05 02:57:01 +0400

    fixed slider

--- components/Testmonial/index.js
+++ components/Testmonial/index.js
@@ -1,16 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
-import { Grid } from '@material-ui/core'
+import { Grid, Hidden } from '@material-ui/core'
 import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
-import { Parallax } from "react-parallax";
-import SectionTitle from "../Title";
-import AcUnitOutlined from "@material-ui/icons/AcUnitOutlined";
-import Camera from "@material-ui/icons/Camera";
-import AccessibilityTwoToneIcon from "@material-ui/icons/AccessibilityTwoTone";
-import AssignmentIndTwoToneIcon from "@material-ui/icons/AccessTimeOutlined";
-import CastForEducationTwoToneIcon from "@material-ui/icons/CastForEducationTwoTone";
-import HomeWorkTwoToneIcon from "@material-ui/icons/HomeWorkTwoTone";
-
 
 const sliders = [
     {
@@ -36,74 +27,43 @@ const settings = {
     autoplay: true,
     autoplaySpeed: 3000,
 };
-const services = [
-    {
-      icon: <AcUnitOutlined />,
-      title: "LOSS PREVENTION",
-    //   content: (
-    //     <>
-    //       Armed security guards are highly trained individuals working in a role
-    //       of immense responsibility. Many are current or ex-military/law
-    //       enforcement officers:
-    //     </>
-    //   ),
-      id: 1,
-    },
-    {
-      icon: <Camera />,
-      title: "PROPERTY PROTECTION",
-      id: 2,
-    },
-    {
-      icon: <AccessibilityTwoToneIcon />,
-      title: "CONTROL SYSTEMS",
-      id: 3,
-    },
-    {
-      icon: <AssignmentIndTwoToneIcon />,
-      title: "FIRE PROTECTION",
-      id: 4,
-    },
-    {
-        icon: <CastForEducationTwoToneIcon />,
-        title: 'BUSINESS PROTECTION',
-        id: 5
-    },
-    {
-        icon: <HomeWorkTwoToneIcon />,
-        title: 'PHYSICAL GUARDING',
-        id: 6
-    },
-  ];
-
-
-const Testmonial = ({ className = '', title, subTitle }) => {
+const Testmonial = ({ className = '' }) => {
     return (
-        <Parallax
-            bgImage="/images/practice/1.jpg"
-            bgImageAlt="the cat"
-            contentClassName={`ourServiceArea ${className}`}
-            strength={200}
-        >
-            <Grid container spacing={4} className="container">
-                <Grid item xs={12}>
-                    <SectionTitle title={title} subTitle={subTitle} />
-                </Grid>
-                {services.map((service, index) => (
-                    <Grid item xs={12} lg={6} sm={6} key={index}>
-                        <Grid className="serviceWrap">
-                            <Grid className="serviceIcon">{service.icon}</Grid>
-                            <Grid className="serviceContent">
-                                <h3>
-                                    <a>{service.title}</a>
-                                </h3>
-                                <p>{service.content}</p>
-                            </Grid>
-                        </Grid>
+        <Grid className={`testmonialArea ${className}`}>
+            <Grid container className="container">
+                <Grid item md={4} sm={6} xs={12}>
+                    <Grid className="testimonialImages">
+                        <img src='/images/testimonial/1.png' alt="" />
                     </Grid>
-                ))}
+                </Grid>
+                <Hidden smDown>
+                    <Grid item md={1}></Grid>
+                </Hidden>
+                <Grid item md={7} sm={6} xs={12}>
+                    <Grid className="testimonialContent">
+                        <span>What People Say</span>
+                        <h2>Client Testimonial</h2>
+                        <Slider className="testmonialSlider"
+                            {...settings}>
+                            {sliders.map((slider, i) => (
+                                <Grid key={i} className="slideItem">
+                                    <p><FormatQuoteIcon /> {slider.text}</p>
+                                    <Grid className="thumbWrap">
+                                        <Grid className="thumbImg">
+                                            <img src={slider.images} alt="" />
+                                        </Grid>
+                                        <Grid className="imgContent">
+                                            <h4>{slider.title}</h4>
+                                            <span>{slider.subTitle}</span>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            ))}
+                        </Slider>
+                    </Grid>
+                </Grid>
             </Grid>
-        </Parallax>
+        </Grid>
     )
 }
 export default Testmonial
